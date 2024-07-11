5cc5d7d27938340751aa0d8b47cebefea8bd6a7f
sdahagam <sdahagam@hiscoinc.com>
2024-07-11 15:42:15 +0530

    changed name

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">Dashboard</Link>
             </li>
             <li>
               <Link to="/about">
