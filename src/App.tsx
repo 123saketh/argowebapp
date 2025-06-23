a7425fa3bc3f432656f0a87b25b5d9a7bfd54f63
123saketh <123saketh@gmail.com>
2025-06-23 23:02:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             </li>
             <li>
               <Link to="/contact">
-                Contact US
+                Contact Me
               </Link>
             </li>
           </ul>
